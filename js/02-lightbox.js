08394c0ccabff35aa9b04078b3e03ac6b567ecb1
dima-zharyy <zharyydimka@gmail.com>
2022-05-14 11:42:29 +0300

    added-task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// ----------------------- Render gallery items -----------------------
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(data) {
+  return data
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+    `;
+    })
+    .join("");
+}
+
+function renderGalleryElements(data) {
+  const galleryMarkup = createGalleryMarkup(data);
+  galleryList.innerHTML = galleryMarkup;
+}
+
+renderGalleryElements(galleryItems);
+
+// ----------------------- SimpleLightbox initialization -----------------------
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
